a2b652c0002dd5133d31a66d083d2da89bc877b7
Henri Ruut <henriruut@Markos-MacBook-PRO.local>
2018-03-31 15:10:04 +0300

    Force push script

---
+++ Assets/Scripts/ForcePush.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePush : MonoBehaviour {
+
+	public float radius;
+	public float power;
+
+	void Start()
+	{
+		
+	}
+
+	void FixedUpdate ()
+	{
+		if (Input.GetKeyDown("x")){
+			
+
+			Vector3 explosionPos = transform.position;
+			Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+
+
+			//Debug.Log ("käib", gameObject);
+			//var hitColliders = Physics2D.OverlapArea(transform.x, transform.y, 1 << LayerMask.NameToLayer("Push items")); //layermask to filter the varius useless colliders
+			//Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+			foreach (Collider2D hit in colliders) {
+				if (hit && hit.GetComponent<Rigidbody2D>())
+					hit.GetComponent<Rigidbody2D>().AddExplosionForce(power, explosionPos, radius, 3.0F);
+
+			}	
+		}	
+
+	}
+}
