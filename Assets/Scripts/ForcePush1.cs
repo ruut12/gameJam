9a48a40eb7c1beb6e288a548dbc98f5a037f5908
Henri Ruut <henriruut@Markos-MacBook-PRO.local>
2018-04-01 11:52:23 +0300

    forcepush delay for player1

--- Assets/Scripts/ForcePush1.cs
+++ Assets/Scripts/ForcePush1.cs
@@ -6,6 +6,10 @@ public class ForcePush1 : MonoBehaviour {
 
 	public float radius;
 	public float power;
+	public float PushCooldown = 1.0f;
+
+	long lasActionTime;
+	public bool isAction = false;
 
 	void Start()
 	{
@@ -14,19 +18,37 @@ public class ForcePush1 : MonoBehaviour {
 
 	void FixedUpdate ()
 	{
+		var nowTicks = System.DateTime.Now.Ticks;
 		if (Input.GetButtonDown("Fire1")) {
+			//			var player = gameObject.GetComponent<Player>();
 
-            FindObjectOfType<AudioManager>().Play("push");
+			if ((lasActionTime + (PushCooldown * 10000000)) < (nowTicks)){
+				isAction = true;
+				lasActionTime = nowTicks; // alusta tegemist
+			}
+			Debug.Log (nowTicks - lasActionTime);
+			if (isAction && (lasActionTime + (PushCooldown * 10000000)) < (nowTicks)){
+				isAction = false; // tee actionit mingi 1 sekund
+			}
 
-            Vector3 explosionPos = transform.position;
-			Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+			if (isAction) {
+				pushItems ();
+			}
 
-			foreach (Collider2D hit in colliders) {
-				if (hit && hit.GetComponent<Rigidbody2D>())
-					hit.GetComponent<Rigidbody2D>().AddExplosionForce(power, explosionPos, radius, 3.0F);
+			isAction = false;
 
-			}	
+		}
+	}
+	void pushItems(){
+		FindObjectOfType<AudioManager>().Play("push");
+
+		Vector3 explosionPos = transform.position;
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+
+		foreach (Collider2D hit in colliders) {
+			if (hit && hit.GetComponent<Rigidbody2D>())
+				hit.GetComponent<Rigidbody2D>().AddExplosionForce(power, explosionPos, radius, 3.0F);
+
 		}	
-
 	}
 }
