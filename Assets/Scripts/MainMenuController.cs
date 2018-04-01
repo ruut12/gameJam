b520c25a21d923ca477af0ba22b52a6c942ffcfc
lsoonvald <lars.soonvald@gmail.com>
2018-04-01 16:31:51 +0300

    main menu tweak

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,18 +7,22 @@ public class MainMenuController : MonoBehaviour {
     public GameState gamePlay;
     public int players, readyPlayers;
     public Text mainInfo, player1Ready, player2Ready, player3Ready, player4Ready;
-    public Image p1Img, p2Img, p3Img, p4Img;
     public int p1ReadyPress = 0;
     public int p2ReadyPress = 0;
     public int p3ReadyPress = 0;
     public int p4ReadyPress = 0;
-    public string[,] chars = new string[7, 2] { { "char1", "-" }, { "char2", "-" } , { "char3", "-" } , { "char4", "-" } , { "char5", "-" }, { "char6", "-" }, { "char7", "-" }};
+    public GameObject p1Img, p2Img, p3Img, p4Img;
+    public string[,] chars = new string[4, 2] { { "char1", "-" }, { "char2", "-" } , { "char3", "-" } , { "char4", "-" }};
 	// Use this for initialization
 	void Start () {
-        p1Img = GameObject.Find("Player1Img").GetComponent<Image>();
-        p2Img = GameObject.Find("Player2Img").GetComponent<Image>();
-        p3Img = GameObject.Find("Player3Img").GetComponent<Image>();
-        p4Img = GameObject.Find("Player4Img").GetComponent<Image>();
+        p1Img = GameObject.Find("Player1Img");
+        p2Img = GameObject.Find("Player2Img");
+        p3Img = GameObject.Find("Player3Img");
+        p4Img = GameObject.Find("Player4Img");
+        p1Img.SetActive(false);
+        p2Img.SetActive(false);
+        p3Img.SetActive(false);
+        p4Img.SetActive(false);
         mainInfo = GameObject.Find("PressAorX").GetComponent<Text>();
         player1Ready = GameObject.Find("Player1Ready").GetComponent<Text>();
         player2Ready = GameObject.Find("Player2Ready").GetComponent<Text>();
@@ -32,7 +36,7 @@ public class MainMenuController : MonoBehaviour {
         if (Input.GetKeyDown("joystick 1 button 0") && p1ReadyPress == 0)
         {
             mainInfo.text = "Press B or X to get ready";
-            p1Img.sprite = Resources.Load<Sprite>(chars[0,0]);
+            p1Img.SetActive(true);
             player1Ready.text = "Not ready";
             p1ReadyPress = 1;
             players++;
@@ -46,7 +50,7 @@ public class MainMenuController : MonoBehaviour {
             if (Input.GetKeyDown("joystick 2 button 0") && p2ReadyPress == 0)
         {
             mainInfo.text = "Press B or X to get ready";
-            p2Img.sprite = Resources.Load<Sprite>(chars[1, 0]);
+            p2Img.SetActive(true);
             player2Ready.text = "Not ready";
             p2ReadyPress = 1;
             players++;
@@ -60,7 +64,7 @@ public class MainMenuController : MonoBehaviour {
         if (Input.GetKeyDown("joystick 3 button 0") && p3ReadyPress == 0)
         {
             mainInfo.text = "Press B or X to get ready";
-            p3Img.sprite = Resources.Load<Sprite>(chars[2, 0]);
+            p3Img.SetActive(true);
             player3Ready.text = "Not ready";
             p3ReadyPress = 1;
             players++;
@@ -74,7 +78,7 @@ public class MainMenuController : MonoBehaviour {
         if (Input.GetKeyDown("joystick 4 button 0") && p4ReadyPress == 0)
         {
             mainInfo.text = "Press B or X to get ready";
-            p4Img.sprite = Resources.Load<Sprite>(chars[3, 0]);
+            p4Img.SetActive(true);
             player4Ready.text = "Not ready";
             p4ReadyPress = 1;
             players++;
