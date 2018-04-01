4cb8f80a769e0b42bde646d335b358e03187dc07
Sixten <s.leemets@gmail.com>
2018-04-01 17:45:40 +0300

    intro sounds

---
+++ Assets/PlayerSelect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelect : MonoBehaviour {
+
+	void Start () {
+        FindObjectOfType<AudioManager>().Play("fury");
+        FindObjectOfType<AudioManager>().Play("intro");
+    }
+
+}
