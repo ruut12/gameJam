debf2fde1093d97dea3af858bbb3fb29b9953524
lsoonvald <lars.soonvald@gmail.com>
2018-04-01 11:52:49 +0300

    menu scene edits

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameState : MonoBehaviour {
+    public Scene game, menu;
+    public string gameSceneName = "4 players";
+    public string gameMenuName = "LarsScene";
     public GameObject levelOne, mainMenu;
     public int playerCount;
 	// Use this for initialization
 	void Start () {
-        levelOne = GameObject.Find("LevelOne");
-        levelOne.SetActive(false);
-        mainMenu = GameObject.Find("MainMenu");
-        mainMenu.SetActive(true);
-
+        game = SceneManager.GetSceneByName(gameSceneName);
+        menu = SceneManager.GetSceneByName(gameMenuName);
+        SceneManager.SetActiveScene(menu);
     }
 
     public void startGame()
@@ -21,8 +23,7 @@ public class GameState : MonoBehaviour {
         int p2ReadyPress = GameObject.Find("MainMenu").GetComponent<MainMenuController>().p2ReadyPress;
         int p3ReadyPress = GameObject.Find("MainMenu").GetComponent<MainMenuController>().p3ReadyPress;
         int p4ReadyPress = GameObject.Find("MainMenu").GetComponent<MainMenuController>().p4ReadyPress;
-        mainMenu.SetActive(false);
-        levelOne.SetActive(true);
+        SceneManager.SetActiveScene(game);
         if(p1ReadyPress == 0)
         {
             GameObject.Find("PlayerOne").SetActive(false);
