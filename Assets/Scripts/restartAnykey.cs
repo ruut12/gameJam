f6f7b4572a4c378b0ebdc6a39bffb772db1a7868
Sixten Leemets <s.leemets@gmail.com>
2018-04-14 18:50:31 +0300

    delay before skipping win scenes

--- Assets/Scripts/restartAnykey.cs
+++ Assets/Scripts/restartAnykey.cs
@@ -5,16 +5,28 @@ public class restartAnykey : MonoBehaviour
 {
     string gameMenuName = "PlayerSelect";
 
+    float timer = 0;
+    float time = 0.7f;
+
     // Use this for initialization
     void Start()
     {
-
+        timer = time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+        }
+
+        if (timer == 0 && Input.anyKeyDown)
         {
             print("any key pressed");
 
